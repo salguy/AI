b8a94bd019cf5e985ba7e12226778f1e81aa0a94
kth0910 <kth_04@naver.com>
2025-04-10 15:14:30 +0900

    Fix: 모델 출력 형식 에러 발생, 프롬프트 수정

--- src/domain/ai/router.py
+++ src/domain/ai/router.py
@@ -18,9 +18,9 @@ async def ai_inference(record: AIInput, request: Request):
         raw = await request.body()
         raw = raw.decode("utf-8")
         print_log(f"🧾 [AI 서버] Raw body: {raw}")
-        print_log(f"response : {record}")
-        print_log(f"input_text : {record.input_text}")
-        print_log(f"scheduleId : {record.scheduleId}")
+        # print_log(f"response : {record}")
+        # print_log(f"input_text : {record.input_text}")
+        # print_log(f"scheduleId : {record.scheduleId}")
         return deliver_to_model(record.input_text, record.scheduleId)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
