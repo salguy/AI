8102e363751ad026c99b2bf7d45c275e186867e2
kth0910 <kth_04@naver.com>
2025-04-10 15:26:49 +0900

    Fix: 필요없는 로그 삭제

--- src/chatbot.py
+++ src/chatbot.py
@@ -123,9 +123,9 @@ SYSTEM_PROMPT = [
 
 def parse_llm_output(text):
     # 1. assistant 시작 위치 찾기
-    print("🧪 [디버깅] 들어온 text 타입:", type(text), flush=True)  # 얘가 먼저 찍힘
-    print("🧪 [디버깅] 들어온 text 길이:", len(text))
-    print("🧪 [디버깅] text 내용 일부:", repr(text[:300]), flush=True)  # 줄바꿈 포함 보이게
+    #print("🧪 [디버깅] 들어온 text 타입:", type(text), flush=True)  # 얘가 먼저 찍힘
+    #print("🧪 [디버깅] 들어온 text 길이:", len(text))
+    #print("🧪 [디버깅] text 내용 일부:", repr(text[:300]), flush=True)  # 줄바꿈 포함 보이게
     start = re.search(r'<\|start_header_id\|>assistant<\|end_header_id\|>', text)
 
     if not start:
