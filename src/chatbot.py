6c3db2c21a76073af2dd102537191b073e0de518
kth0910 <kth_04@naver.com>
2025-05-14 19:58:38 +0900

    hotfix: medicine notification prompt일 때 json 파싱 오류 수정

--- src/chatbot.py
+++ src/chatbot.py
@@ -288,11 +288,9 @@ def chat_with_llm(datasets, custom_prompt=None):
             print_log(f'사용자의 응답: {data_input}')
             print_log(f'AI의 응답: {output_text}')
             if result:
-                if custom_prompt != INTENT_INFERENCE_PROMPT:
+                if custom_prompt != MEDICINE_NOTIFICATION_PROMPT and custom_prompt != INTENT_INFERENCE_PROMPT:
                     print_log(f'JSON: {result["json"]}')
                     print_log(f'응답: {result["response"]}')
-
-                if custom_prompt != MEDICINE_NOTIFICATION_PROMPT and custom_prompt != INTENT_INFERENCE_PROMPT:
                     json_data = safe_json_load(result["json"])
                     if json_data is None:
                         print_log("JSON이 None이므로 파싱 실패!", 'error')
